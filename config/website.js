393706c52afaba17657d7a0d36bda9520152cb3e
Sinisa Colic <sinisa820@gmail.com>
2019-05-24 19:03:14 +0200

    helmet done

---
+++ config/website.js
@@ -0,0 +1,19 @@
+module.exports = {
+	pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+
+	siteTitle: 'React Parallax - Gatsby Portfolio', // Navigation and Site Title
+	siteTitleAlt: 'Landing Parallax', // Alternative Site title for SEO
+	siteTitleShort: 'Landing Parallax', // short_name for manifest
+	siteHeadline: 'Creating very fast websites', // Headline for schema.org JSONLD
+	siteUrl: 'https://priceless-hypatia-841602.netlify.com', // Domain of your site. No trailing slash!
+	siteLanguage: 'en', // Language Tag on <html> element
+	siteLogo: '/logo.png', // Used for SEO and manifest
+	siteDescription: 'Built with React and some parallax effects',
+	author: 'Sinisa Colic', // Author for schema.org JSONLD
+
+	// siteFBAppID: '123456789', // Facebook App ID - Optional
+
+	// Manifest and Progress color
+	themeColor: 'blue',
+	backgroundColor: 'orange'
+}
